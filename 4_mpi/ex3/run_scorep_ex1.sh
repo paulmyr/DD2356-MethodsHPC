40b216a7491b858a5e46db4786fda6364ad31d7b
Rishi Vijayvargiya <rishivijayv@gmail.com>
2025-05-13 13:47:55 +0200

    feat(a4,ex3): adding scorep results for ex1

---
+++ 4_mpi/ex3/run_scorep_ex1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+# The -l above is required to get the full environment with modules
+
+# Set the allocation to be charged for this job
+# not required if you have set a default allocation
+#SBATCH -A edu25.DD2356
+
+# The name of the script
+#SBATCH -J dd2356_a4_ex3_problem1
+
+# The partition
+#SBATCH -p shared
+
+# 10 hours wall clock time will be given to this job
+#SBATCH -t 2:00:00
+
+# Total number of MPI tasks (processes)
+#SBATCH --ntasks=32                 
+
+# Number of nodes
+#SBATCH --nodes 8
+
+# Number of MPI processes per node
+#SBATCH --ntasks-per-node 4
+
+# CPUs per task
+#SBATCH --cpus-per-task 1
+
+# Error file
+#SBATCH -e elog_dd2356_a4_ex3_problem1
+
+# Output File
+#SBATCH --output a4_ex3_problem1.txt
+
+# Run the executable named myexe
+# and write the output into my output file
+srun ./halo_parallel.out 64000
