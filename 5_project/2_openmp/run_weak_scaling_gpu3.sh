d0b88b0cc91eeff6c4dc263117663818cb575cc7
Paul Mayer <pmayer@kth.se>
2025-05-25 00:40:43 +0200

    [project] omp weak and strong scaling, gpu run scripts

--- 5_project/2_openmp/run_weak_scaling_gpu3.sh
+++ 5_project/2_openmp/run_weak_scaling_gpu3.sh
@@ -2,29 +2,30 @@
 # The -l above is required to get the full environment with modules
 
 #SBATCH -A edu25.DD2356
-#SBATCH -J g18pr_omp_weak_scaling
-#SBATCH -p shared
+#SBATCH -J grp18pr_gpu_weak_scaling
+#SBATCH -p gpu
 #SBATCH -t 1:00:00
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
-#SBATCH --cpus-per-task 256
-#SBATCH -e elog_project_omp_weak_scaling
-#SBATCH --output project_omp_weak_scaling.txt
+#SBATCH --cpus-per-task 1
+#SBATCH --gpus-per-task 3
+#SBATCH -e elog_project_gpu_weak_scaling3
+#SBATCH --output project_gpu_weak_scaling3.txt
 
 # This is the size of the subproblem PER PROCESS (remains fixed)
 subprocess_problem=100000
 echo "WEAK SCALING: SIZE PER PROBLEM IS $subprocess_problem"
 # Run the executable named myexe
 # and write the output into my output file
-for processes in 1 2 4 8 16 32 64 128 256
+##for processes in 1 2 4 8 16 32 64 128 256
+for processes in 3
 do
     grid_size=$((subprocess_problem * processes))
-    export OMP_NUM_THREADS=$processes
-    echo "########## RUNNING WITH $processes THREADS ##########"
+    echo "########## RUNNING WITH $processes GPUS ##########"
     for count in {1..3}
     do
         echo "Run $count"
-        srun -n 1 omp.out $grid_size
+        srun -n 1 gpu.out $grid_size
         echo "----------------------------"
     done
 done
